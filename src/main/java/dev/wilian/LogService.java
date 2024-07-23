b7a485cde75913f396a0ebc14a8b77097d66d778
wilian <wilianhenrique.souza94@gmail.com>
2024-07-23 19:26:26 -0300

    Adjusts

--- src/main/java/dev/wilian/LogService.java
+++ src/main/java/dev/wilian/LogService.java
@@ -41,7 +41,7 @@ public class LogService {
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorServer.class.getSimpleName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
 
         return properties;
     }
